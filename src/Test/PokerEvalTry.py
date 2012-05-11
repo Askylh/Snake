942c319fcb34ba6f994eea22dcb307b3bc2b99b5
Chris <chris@Bahamas.(none)>
2012-05-11 10:50:54 +0200

    Création du fichier Poker evla try

---
+++ src/Test/PokerEvalTry.py
@@ -0,0 +1,14 @@
+'''
+Created on 11 mai 2012
+
+@author: Askylh
+'''
+
+from pokereval import PokerEval
+
+
+Peval = PokerEval()
+iteration=100000
+a = Peval.poker_eval(game='holdem', pockets=[["tc", "ts"], ["kd", "ad"]], board=["__", "__", "__", "__", "__"], iterations=iteration)
+
+print a['eval']
