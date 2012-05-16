34c84c4cd3291f1e910e35c6eac8b7817a8038f0
Chris <chris@chicoutimi.(none)>
2012-05-16 22:06:00 +0200

    add pymouse

---
+++ src/Test/mouse/pymouseTest.py
@@ -0,0 +1,58 @@
+#-*-coding: utf-8 *-*
+'''
+Created on 15 mai 2012
+
+@author: Askylh Snake
+'''
+
+from pymouse import PyMouse
+import random, time
+try:
+    from pymouse import PyMouseEvent
+
+    class event(PyMouseEvent):
+        def move(self, x, y):
+            print "Mouse moved to", x, y
+
+        def click(self, x, y, button, press):
+            if press:
+                print "Mouse pressed at", x, y, "with button", button
+            else:
+                print "Mouse released at", x, y, "with button", button
+
+    e = event()
+    #e.capture = True
+    e.start()
+
+except ImportError:
+    print "Mouse events are not yet supported on your platform"
+
+m = PyMouse()
+try:
+    size = m.screen_size()
+    print "size: %s" % (str(size))
+
+    pos = (random.randint(0, size[0]), random.randint(0, size[1]))
+except:
+    pos = (random.randint(0, 250), random.randint(0, 250))
+
+print "Position: %s" % (str(pos))
+
+m.move(pos[0], pos[1])
+
+time.sleep(2)
+
+m.click(pos[0], pos[1], 1)
+
+time.sleep(2)
+
+m.click(pos[0], pos[1], 2)
+
+time.sleep(2)
+
+m.click(pos[0], pos[1], 3)
+
+try:
+    e.stop()
+except:
+    pass
